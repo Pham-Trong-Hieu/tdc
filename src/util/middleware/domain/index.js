8ca0f63d0305c9d9ba64f34a8c7effb69b81b2f4
phamhieu <20062501.hieu@student.iuh.edu.vn>
2024-08-05 15:23:28 +0700

    push

--- src/util/middleware/domain/index.js
+++ src/util/middleware/domain/index.js
@@ -20,26 +20,10 @@ function confirmDomain(req, res, next) {
 
   console.log(reqDomain);
   domain.checkDomain(reqDomain, (err, results, fields) => {
-    if (err) {
-      return res.send(
-        `<div style = "box-sizing: border-box;font-family: 'Source Code Pro', monospace; height: 90vh;display: flex;align-items: center;justify-content: center;" = >Đang bảo trì...</div>`
-      );
-    } else {
-      let rows = results.map((row) => {
-        return row;
-      });
 
-      if (rows.length == 0) {
-        return res.send(
-          `<div style = "box-sizing: border-box;font-family: 'Source Code Pro', monospace; height: 90vh;display: flex;align-items: center;justify-content: center;" = >Đang bảo trì...</div>`
-        );
-      }
-      if (rows[0].disabled == 1) {
-        return res.send(
-          "Opp!, Có gì đó không ổn! </br>Có vẻ như domain hiện tại đã bị vô hiệu bởi nhà cung cấp dịch vụ.<br>Liên hệ MIDVN để được hỗ trợ."
-        );
-      }
-      const dbName = rows[0].database_name;
+   
+    
+      const dbName = 'bafzmishkw4tibaxnamp';
 
       if (!pool[dbName]) {
         const dbConfig = {
@@ -47,18 +31,18 @@ function confirmDomain(req, res, next) {
           port: "3306",
           user: "usz36ahjbnmtbqyn",
           password: "1YrvUF4TgWRHA1NnBcZT",
-          database: 'bafzmishkw4tibaxnamp',
+          database: dbName,
           multipleStatements: true,
           charset: "utf8mb4",
         };
         pool[dbName] = mysql.createPool(dbConfig);
       }
 
-      req.dbName = rows[0].database_name;
+      req.dbName = dbName;
       req.reqDomain = reqDomain;
 
       next();
-    }
+   
   });
 }
 module.exports = confirmDomain;
